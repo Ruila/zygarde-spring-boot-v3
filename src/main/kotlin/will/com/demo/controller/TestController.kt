83b4ff3330f4f956191607c3704533d5e9da22ad
Ruila <4momolmr@gmail.com>
2023-03-23 17:45:19 +0800

    feat: modify example

--- src/main/kotlin/will/com/demo/controller/TestController.kt
+++ src/main/kotlin/will/com/demo/controller/TestController.kt
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import will.com.demo.data.dao.RoleDao
+import will.com.demo.entity.search.name
+import zygarde.data.jpa.dao.searchOne
+
 @RestController
 @RequestMapping
 class TestController(
@@ -11,8 +14,8 @@ class TestController(
 ) {
     @GetMapping("/data")
     fun getData(): String {
-        val data = roleDao.findAll()
-        return data.first().name
+        val data = roleDao.searchOne { name() eq "root" }
+        return data?.description ?: "empty"
     }
 
 }
